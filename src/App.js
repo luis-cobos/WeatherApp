297ff11fc2bc261a159f72e736901baa7b728441
luis-cobos <48224442+luis-cobos@users.noreply.github.com>
2019-04-15 14:58:03 -0500

    Full Aplication missing Search Function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component } from 'react';
+import {Grid, Col, Row} from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+
+import LocationList from './components/LocationList';
+import ForecastExtended from './components/ForecastExtended';
+import SearchByCity from './components/Search';
 import './App.css';
 
+const cities = [
+  'Aguascalientes',
+  'Ciudad de México,mx',
+  'Jalisco',
+  'Monterrey',
+  'California,us',
+  'Texas,us',
+];
+
 class App extends Component {
+
+  constructor (){
+    super();
+    this.state= {city: null};
+  }
+
+  handleSelectedLocation = city => {
+    this.setState({city}); 
+    console.log(`handleSelectedLocation ${city}`);
+
+  }
+
   render() {
+    const {city } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+        <Grid>
+          <Row>
+                  <SearchByCity />
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+            <AppBar position='sticky'>
+              <Toolbar>
+                <Typography variant='title' color='inherit'>
+                  Pronostico Actual
+                </Typography>
+              </Toolbar>
+            </AppBar>
+              <LocationList 
+                cities={cities} 
+                onSelectedLocation={this.handleSelectedLocation} >
+              </LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+            <AppBar position='sticky'>
+              <Toolbar>
+                <Typography variant='title' color='inherit'>
+                  Pronostico Extendido
+                </Typography>
+              </Toolbar>
+            </AppBar>
+              <Paper elevation={4}>
+                <div className="details">
+
+                {city && 
+                  <ForecastExtended city={city}></ForecastExtended>
+
+                  }
+
+                </div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
     );
   }
 }
---
+++ src/components/WeatherLocation/Location.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+import './styles.css';
+
+
+const Location = ({city}) => (
+    //const city = props.city;
+
+    //Destructuring
+    //const {city} = props;
+    <div className="locationCont">
+        <h1>
+            {city}
+        </h1>
+    </div>
+    );
+
+    
+Location.propTypes = {
+    city: PropTypes.string.isRequired,
+}
+
+export default Location;
